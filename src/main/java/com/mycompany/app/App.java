26e562cbfd0c19affdc2c8d74f672861e46ba4d2
jegatheshgit <41848419+jegatheshgit@users.noreply.github.com>
2018-08-12 12:30:52 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World jenkinsgitintegration updated!";
+    private final String message = "Hello World jenkinsgitintegration updated1!";
 
     public App() {}
 
